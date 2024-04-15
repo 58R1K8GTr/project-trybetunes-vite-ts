311f079351d47f3017e81ec046c7ecb74e30a41e
no_name <no@email.com>
2024-04-14 22:03:14 -0300

    Requisito 1 satisfeito

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createUser } from '../../services/userAPI';
+import Loading from '../Loading';
+
+function Login() {
+  const [disabled, setDisabled] = useState(true);
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  let html;
+
+  function handleChange({ target: { value } }: React.ChangeEvent<HTMLInputElement>) {
+    setDisabled(value.length < 3);
+    setName(value);
+  }
+
+  async function handleClick() {
+    setLoading(true);
+    await createUser({ name });
+    navigate('/search');
+  }
+
+  if (loading) {
+    html = (<Loading />);
+  } else {
+    html = (
+      <div className="login-container">
+        <input
+          onChange={ (event) => handleChange(event) }
+          type="text"
+          data-testid="login-name-input"
+        />
+        <button
+          disabled={ disabled }
+          onClick={ handleClick }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
+  }
+
+  return html;
+}
+
+export default Login;
