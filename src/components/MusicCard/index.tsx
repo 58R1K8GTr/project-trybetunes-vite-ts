0886d4ab14ae15312a8f199eae3cd9372637b405
no_name <no@email.com>
2024-04-17 21:00:17 -0300

    Requisito 5 satisfeito

--- src/components/MusicCard/index.tsx
+++ src/components/MusicCard/index.tsx
@@ -1,25 +1,54 @@
+import { useState } from 'react';
 import { SongType } from '../../types';
+import checkedHeart from '../../images/checked_heart.png';
+import emptyHeart from '../../images/empty_heart.png';
 
 function MusicCard({ info }: { info: SongType }) {
   const { trackId, trackName, previewUrl } = info;
   const idAudio = `${trackName}${trackId}`;
+  const [favorite, setFavorite] = useState(false);
+  const [image, setImage] = useState<string>(emptyHeart);
+
+  function handleChange({ target: { checked } }: React.ChangeEvent<HTMLInputElement>) {
+    setFavorite(checked);
+    if (checked) {
+      setImage(checkedHeart);
+    } else {
+      setImage(emptyHeart);
+    }
+  }
 
   return (
-    <label htmlFor={ idAudio }>
-      { trackName }
-      <audio
-        id={ idAudio }
-        data-testid="audio-component"
-        src={ previewUrl }
-        controls
+    <>
+      <label htmlFor={ idAudio }>
+        { trackName }
+        <audio
+          id={ idAudio }
+          data-testid="audio-component"
+          src={ previewUrl }
+          controls
+        >
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </label>
+      <label
+        data-testid={ `checkbox-music-${trackId}` }
+        htmlFor={ `checkbox-music-${trackId}` }
       >
-        <track kind="captions" />
-        O seu navegador não suporta o elemento
-        {' '}
-        <code>audio</code>
-        .
-      </audio>
-    </label>
+        <input
+          checked={ favorite }
+          type="checkbox"
+          name="favorite"
+          id={ `checkbox-music-${trackId}` }
+          onChange={ (event) => handleChange(event) }
+        />
+        <img src={ image } alt="favorite" />
+      </label>
+    </>
   );
 }
 
